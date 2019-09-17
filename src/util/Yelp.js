6163656edf9707746ae689c6744315e01c05b731
Asylbek Ismagambetov <ismagambetov@gmail.com>
2019-09-16 20:54:42 -0400

    Finished final Part 4. Application is working with YelpAPI.

---
+++ src/util/Yelp.js
@@ -0,0 +1,37 @@
+const apiKey =
+  "4v9KOaV9JybJOTR1ik4oYTP9CJ1CR5IeHhhrhShsw0Q98UiZdbwaCCNA9aEacepBnskE_pipSi1Ni8foETICkcRuojhRLEnW6WXJHje9a24UGNjvLsayTqbJKRqAXXYx";
+
+const yelp = {
+  search(term, location, sortBy) {
+    return fetch(
+      `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+      {
+        headers: { Authorization: `Bearer ${apiKey}` }
+      }
+    )
+      .then(response => {
+        return response.json();
+      })
+      .then(jsonResponse => {
+        if (jsonResponse.businesses) {
+          return jsonResponse.businesses.map(business => {
+            return {
+              id: business.id,
+              imageSrc: business.image_url,
+              name: business.name,
+              address: business.location.address1,
+              city: business.location.city,
+              state: business.location.state,
+              zipCode: business.location.zip_code,
+              category: business.categories[0].title,
+              rating: business.rating,
+              reviewCount: business.review_count
+            };
+          });
+        }
+      });
+  }
+};
+
+
+export default yelp;
